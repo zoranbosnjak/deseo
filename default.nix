34fac61d63dd43866f8532aca46ffa80e5b8fa42
Zoran Bošnjak <zoran.bosnjak@sloveniacontrol.si>
2018-05-30 11:55:20 +0200

    bump version, switch to SemVer versioning scheme

--- default.nix
+++ default.nix
@@ -5,7 +5,7 @@
 }:
 mkDerivation {
   pname = "deseo";
-  version = "1.3.0.1";
+  version = "1.4.0";
   src = ./.;
   libraryHaskellDepends = [
     base bytestring containers deepseq deepseq-generics megaparsec mtl
